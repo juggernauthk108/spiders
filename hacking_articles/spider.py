d3b568e383f4bd9d7ccd49686ebf8b6c771be864
einvoiceserver03 <tejas.zarekar@zycus.com>
2019-03-21 20:07:55 +0530

    python script

--- hacking_articles/spider.py
+++ hacking_articles/spider.py
@@ -2,10 +2,14 @@
 import elasticsearch
 print "Welcome to the hacking hood"
 
-
-
-es = E
-
-
+counter=0
+fname="hack_list"
+with open(fname) as f:
+    content=f.readlines()
+    for line in content:
+        print line
+        counter+=1
+        print counter
+        
 
 
