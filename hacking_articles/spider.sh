0f44918c206e36d517e4b886509b002d14288d3b
einvoiceserver03 <tejas.zarekar@zycus.com>
2019-03-21 19:01:35 +0530

    added elastic search API queries

--- hacking_articles/spider.sh
+++ hacking_articles/spider.sh
@@ -3,5 +3,6 @@ for i in `cat hack_list`:
 do
 	NAME=$(echo $i | rev | cut -f2 -d\/ | rev)
 	echo "Going for $NAME"
-	curl -s -k $i | xmllint --html --xpath "//article//text()" - 2> /dev/null > output/$NAME
+	url=$(echo $i)
+	content=$(curl -s -k $i | xmllint --html --xpath "//article//text()" - 2> /dev/null)
 done
