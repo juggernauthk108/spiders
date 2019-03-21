054816f82cba7a2613a0ff116a9852892d31d070
einvoiceserver03 <tejas.zarekar@zycus.com>
2019-03-21 19:53:20 +0530

    added a python script

---
+++ hacking_articles/spider.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import elasticsearch
+print "Welcome to the hacking hood"
+
+
+
+es = E
+
+
+
+
--- hacking_articles/spider.sh
+++ hacking_articles/spider.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-for i in `cat hack_list`:
+counter=0
+for i in `cat hack_list`
 do
 	NAME=$(echo $i | rev | cut -f2 -d\/ | rev)
 	echo "Going for $NAME"
 	url=$(echo $i)
 	content=$(curl -s -k $i | xmllint --html --xpath "//article//text()" - 2> /dev/null)
+	./populatingMapping $url $content $counter
+	counter=$((counter+1))
 done
